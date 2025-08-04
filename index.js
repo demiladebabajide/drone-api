c7e0c2993e8e18c5748d53fc3557fd8256812fac
demi.babajide <20555807+demiladebabajide@users.noreply.github.com>
2025-08-04 18:54:56 +0100

    Project structure

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
+const port = 8000;
 const app = express();
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 mongoose.connect("mongodb://localhost:27017/drone-api", {
     useNewUrlParser: true,
@@ -9,8 +14,8 @@ mongoose.connect("mongodb://localhost:27017/drone-api", {
 })
 .then(() => {
     console.log("Connected to MongoDB successfully");
-     app.listen(8000, () => {
-        console.log(`Server is running on http://localhost:8000`);
+     app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
     });
 })
 .catch((err) => {
@@ -18,8 +23,17 @@ mongoose.connect("mongodb://localhost:27017/drone-api", {
 });
 
 
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get("/", (req, res) => {
   res.status(200).json({ alive: "True" });
 });
+
+app.use('/api', require('./routes/drone.route'));
 
 module.exports = app;
